c93a5bfaa95b743165b1ee5c237b71ce461b1f50
Eddy Rodriguez <ext_eddrodriguez@falabella.cl>
2022-04-06 12:35:04 -0500

    Initial setup: express, dotenv, mongoose

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config({
+    path: '.env',
+});
+
+const app = express();
+app.use(express.json());
+
+app.listen(process.env.PORT || 8000, () => {
+    mongoose.connect(process.env.DB_CONNECTION_STRING_URI)
+        .then(() => console.log(`Successfully connected to DB <{${process.env.DB_CONNECTION_STRING_URI}}>`))
+        .catch(err => console.log(`Couldn't connect to DB <{${process.env.DB_CONNECTION_STRING_URI}}>. Error: ${err}`));
+});
+
+module.exports = app;
